2c5fdc62e530177ea1ea5a0f57a70916f0ef1888
konrad <konrad@e0d02828-37e6-11de-b438-597f59cd7555>
2008-11-08 21:29:08 +0000

    * CSS test added. * Updates to the CSS rendering. Still buggy.

--- hyperlight/languages/css.php
+++ hyperlight/languages/css.php
@@ -12,14 +12,16 @@ class CssLanguage extends HyperLanguage {
             )
         ));
 
-        $nmstart = '[a-z0-9]';
+        // The following does not conform to the specs but it is necessary
+        // else numbers wouldn't be recognized any more.
+        $nmstart = '-?[a-z]';
         $nmchar = '[a-z0-9-]';
         $hex = '[0-9a-f]';
         list($string, $strmod) = preg_strip(Rule::STRING);
         $strmod = implode('', $strmod);
 
         $this->addStates(array(
-            'init' => array('comment', 'meta', 'id', 'class', 'block', 'string'),
+            'init' => array('comment', 'meta', 'id', 'class', 'pseudoclass', 'element', 'block', 'string'),
             'block' => array('comment', 'identifier', 'string', 'color', 'number', 'uri'),
         ));
 
@@ -28,15 +30,21 @@ class CssLanguage extends HyperLanguage {
             'meta' => "/@$nmstart$nmchar*/i",
             'id' => "/#$nmstart$nmchar*/i",
             'class' => "/\.$nmstart$nmchar*/",
+            'pseudoclass' => "/:$nmstart$nmchar*/",
+            'element' => "/$nmstart$nmchar*/i",
             'block' => new Rule('/\{/', '/\}/'),
+            'number' => '/[+-]?(?:\d+(\.\d+)?|\d*\.\d+)(%|em|ex|px|pt|in|cm|mm|pc|deg|g?rad|m?s|k?Hz)?/',
+            'uri' => "/url\(\s*(?:$string|[^\)]*)\s*\)/$strmod",
             'identifier' => "/$nmstart$nmchar*/i",
             'string' => "/$string/$strmod",
             'color' => "/#$hex{3}(?:$hex{3})?/i",
-            'number' => '/[+-]?(?:\d+|\d*\.\d+)(%|em|ex|px|pt|in|cm|mm|pc|deg|g?rad|m?s|k?Hz)?/',
-            'uri' => "/url\(\s*(?:$string|[^\)]*)\s*\)/$strmod",
         ));
 
         $this->addMappings(array(
+            'element' => 'keyword',
+            'id' => 'keyword type',
+            'class' => 'keyword literal',
+            'pseudoclass' => 'keyword operator',
             'block' => '',
             'color' => 'string',
         ));
