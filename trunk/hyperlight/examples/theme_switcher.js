f5299bf75b65f9f807d96ab55ee2775606f8c79f
konrad <konrad@e0d02828-37e6-11de-b438-597f59cd7555>
2008-12-10 19:14:06 +0000

    Nifty effect.

--- trunk/hyperlight/examples/theme_switcher.js
+++ trunk/hyperlight/examples/theme_switcher.js
@@ -2,7 +2,17 @@ $(document).ready(function() {
     $('#switch-buttons a').each(function (i, btt) {
         $(btt).click(function () {
             $('#switch-buttons a').each(function (i, btt) { $(btt).removeClass('active'); });
-            $('link#theme').attr({href: '../colors/' + this.id.replace('theme-', '') + '.css'});
+            var cssPath = '../colors/' + this.id.replace('theme-', '') + '.css';
+            var cssAlreadyLoaded = false;
+            $('pre.source-code').each(function (j, code) {
+                $(code).fadeOut('normal', function() {
+                    if (!cssAlreadyLoaded) {
+                        cssAlreadyLoaded = true;
+                        $('link#theme').attr({href: cssPath});
+                    }
+                });
+            });
+            $('pre.source-code').each(function (j, code) { $(code).fadeIn('normal'); });
             $(this).addClass('active');
             return false;
         });
